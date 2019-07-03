87137357eb1333f260facb9e6b74eedad17d2ea0
KFad <kfadigan@yahoo.com>
2019-07-03 11:27:24 -0400

    Wire up controller to display API

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,17 @@
 class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
 
+  def create
+    @product = Product.create(product_params)
+    @product.save
+    redirect_to product_path(@product)
+  end
+
+private
+
+  def product_params
+    params.require(:product).permit(:name, :price, :inventory, :description )
+  end
 end
